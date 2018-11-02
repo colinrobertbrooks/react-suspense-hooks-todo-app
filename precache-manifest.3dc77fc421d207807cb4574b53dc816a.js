b37fe22905f4fc6029b79b7f5c7b15cd087cf32b
colinrcummings <colinrcummings@gmail.com>
2018-11-02 15:15:37 -0400

    Updates

---
+++ precache-manifest.3dc77fc421d207807cb4574b53dc816a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6a2c1228c43a5c0b14a",
+    "url": "/react-suspense-hooks-todo-app/static/js/runtime~main.e6a2c122.js"
+  },
+  {
+    "revision": "dd2c5261bd2a8222bce5",
+    "url": "/react-suspense-hooks-todo-app/static/js/main.dd2c5261.chunk.js"
+  },
+  {
+    "revision": "5fdcf32b48816653a9f9",
+    "url": "/react-suspense-hooks-todo-app/static/js/TodoListPage.5fdcf32b.chunk.js"
+  },
+  {
+    "revision": "bd50523cb9ae8d3f61ad",
+    "url": "/react-suspense-hooks-todo-app/static/js/TodoDetailsPage.bd50523c.chunk.js"
+  },
+  {
+    "revision": "0de34ba0118bd8b7c779",
+    "url": "/react-suspense-hooks-todo-app/static/js/4.0de34ba0.chunk.js"
+  },
+  {
+    "revision": "a7ad627e56ee8cdf20b4",
+    "url": "/react-suspense-hooks-todo-app/static/js/0.a7ad627e.chunk.js"
+  },
+  {
+    "revision": "dd2c5261bd2a8222bce5",
+    "url": "/react-suspense-hooks-todo-app/static/css/main.b6b75814.chunk.css"
+  },
+  {
+    "revision": "0de34ba0118bd8b7c779",
+    "url": "/react-suspense-hooks-todo-app/static/css/4.d7f779c2.chunk.css"
+  },
+  {
+    "revision": "075c77967ed9df87aae5bc16ed5cfd09",
+    "url": "/react-suspense-hooks-todo-app/index.html"
+  }
+];
