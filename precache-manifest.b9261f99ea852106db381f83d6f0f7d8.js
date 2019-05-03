a3920eb37027d9ba7f81c52811cd1b14cbd7e35f
Colin Cummings <colin_cummings@brown.edu>
2019-05-03 19:17:13 -0400

    Updates

---
+++ precache-manifest.b9261f99ea852106db381f83d6f0f7d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38faaf6504177284a7c67a7140e2b079",
+    "url": "/react-suspense-hooks-todo-app/index.html"
+  },
+  {
+    "revision": "9afc92acd683c7faaeef",
+    "url": "/react-suspense-hooks-todo-app/static/css/6.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "0a23185e35c372397a58",
+    "url": "/react-suspense-hooks-todo-app/static/css/main.4f856431.chunk.css"
+  },
+  {
+    "revision": "9afc92acd683c7faaeef",
+    "url": "/react-suspense-hooks-todo-app/static/js/6.27ffba32.chunk.js"
+  },
+  {
+    "revision": "3e34a3fdd507d98540ce",
+    "url": "/react-suspense-hooks-todo-app/static/js/TodoCreatePage.158e0927.chunk.js"
+  },
+  {
+    "revision": "e25ac7686d33078d2a79",
+    "url": "/react-suspense-hooks-todo-app/static/js/TodoDetailsPage.0948f897.chunk.js"
+  },
+  {
+    "revision": "84a5f2f95b534a04d7e8",
+    "url": "/react-suspense-hooks-todo-app/static/js/TodoListPage.99306f81.chunk.js"
+  },
+  {
+    "revision": "3a38752d01007357b6d6",
+    "url": "/react-suspense-hooks-todo-app/static/js/TodoUpdatePage.4e6e9f5f.chunk.js"
+  },
+  {
+    "revision": "0a23185e35c372397a58",
+    "url": "/react-suspense-hooks-todo-app/static/js/main.d85cc5ae.chunk.js"
+  },
+  {
+    "revision": "f534b9e65c7bad1610ca",
+    "url": "/react-suspense-hooks-todo-app/static/js/runtime~main.73432e85.js"
+  }
+]);
