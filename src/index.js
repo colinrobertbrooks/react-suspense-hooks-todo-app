9aa7190ba2a2388dc77f2fea10cda62e5dae8518
Colin Cummings <colin_cummings@brown.edu>
2019-11-15 14:01:45 -0500

    experimental react; remove react-cache

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import Root from './components/Root';
 import './index.css';
 
-ReactDOM.unstable_createRoot(document.getElementById('root')).render(<Root />);
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
