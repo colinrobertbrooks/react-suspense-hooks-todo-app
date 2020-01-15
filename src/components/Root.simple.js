109bbba4d6a661c8d3878ecad8960f5d08b2f46d
Colin Cummings <colinrcummings@Colins-MacBook-Pro.local>
2020-01-15 15:57:41 -0500

    add context, router & some styling

--- src/components/Root.simple.js
+++ src/components/Root.simple.js
@@ -1,27 +1,53 @@
-import React, { Suspense } from 'react';
+import { Router } from '@reach/router';
+import React, { Suspense, createContext } from 'react';
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+import { makeTo } from '../utils/router';
 import resources from '../resources';
+import IsLoading from './IsLoading';
 
-const Loader = () => <p>Loading...</p>;
-
-const TodoList = ({ resource }) => {
+const TodoListGroup = ({ resource }) => {
   const todos = resource.read();
 
   return (
-    <ul>
+    <ListGroup>
       {todos.map(({ id, text }) => (
-        <li key={id}>{text}</li>
+        <ListGroupItem key={id}>{text}</ListGroupItem>
       ))}
-    </ul>
+    </ListGroup>
   );
 };
 
+const TodoListPage = () => (
+  <Row>
+    <Col xs={12} className="text-center">
+      <h1>Todos</h1> <hr />
+      <Suspense fallback={<IsLoading message="Loading todos..." />}>
+        <TodoListGroup resource={resources.todos.list} />
+      </Suspense>
+    </Col>
+  </Row>
+);
+
+const App = () => (
+  <Container>
+    <Router>
+      <TodoListPage path={makeTo('/')} />
+    </Router>
+  </Container>
+);
+
+const TodoContext = createContext(null);
+
+const TodoProvider = ({ children }) => {
+  const value = {};
+
+  return <TodoContext.Provider value={value}>{children}</TodoContext.Provider>;
+};
+
 const Root = () => (
-  <>
-    <h1>Todos</h1>
-    <Suspense fallback={<Loader />}>
-      <TodoList resource={resources.todos.list} />
-    </Suspense>
-  </>
+  <TodoProvider>
+    <App />
+  </TodoProvider>
 );
 
 export default Root;
